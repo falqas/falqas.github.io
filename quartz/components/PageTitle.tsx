84fc033a7be1f0d2600176f7cafd68ff8997d0ac
Faisal Al Qasimi <falqas@gmail.com>
2024-06-12 12:18:01 -0400

    Styling tweaks on mobile

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,12 +7,12 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <>
-      <img src="/static/bios.png" />
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <h1 class={classNames(displayClass, "page-title")}>
         <a href={baseDir}>{title}</a>
       </h1>
-    </>
+      <img src="/static/bios.png" width="220" />
+    </div>
   )
 }
 
